cc01d4c1052e2534caa47b5ee08446343f6d8c77
etasbyjh <jlee@sgh.com>
2016-09-09 14:41:52 -0400

    090916 cal part

---
+++ SGHParetoFrontier/sParetoDataItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHParetoFrontier
+{
+    public class sParetoDataItem
+    {
+        public string name { get; set; }
+        public double value { get; set; }
+        public double value_Normalized { get; set; }
+    }
+}
