b0493e80984129490386be6cce7506ed87775c61
etasbyjh <jlee@sgh.com>
2016-09-09 16:26:58 -0400

    090916 normalizing

--- SGHParetoFrontier/sParetoDataItem.cs
+++ SGHParetoFrontier/sParetoDataItem.cs
@@ -9,7 +9,24 @@ namespace SGHParetoFrontier
     public class sParetoDataItem
     {
         public string name { get; set; }
+        public bool IsNegationRequired { get; set; } = false;
         public double value { get; set; }
         public double value_Normalized { get; set; }
+
+        public double getValueForCompare(bool useNormalized = false)
+        {
+            double val = this.value;
+            if (useNormalized)
+            {
+                val = this.value_Normalized;
+            }
+
+            if (this.IsNegationRequired)
+            {
+                val *= -1;
+            }
+
+            return val;
+        }
     }
 }
