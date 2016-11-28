cc409ca85623ab99d4b550b38a501be251b2348a
etasbyjh <jlee@sgh.com>
2016-11-28 09:23:39 -0500

    161128 JH commit

---
+++ SGHParetoFrontier/sParetoParameterSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGHParetoFrontier
+{
+    public class sParetoParameterSet
+    {
+        public string paramName { get; set; }
+        public List<double> data { get; set; }
+
+        public sParetoParameterSet()
+        {
+            this.paramName = "";
+            this.data = new List<double>();
+        }
+
+        public sParetoParameterSet(string name, List<double> dataIn)
+        {
+            this.paramName = name;
+            this.data = dataIn.ToList();
+        }
+
+    }
+}
